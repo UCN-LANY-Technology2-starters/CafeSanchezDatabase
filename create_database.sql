8f92eea41b6779f9946f6dac5ac23571761e1345
Lars Nysom <LANY@ucn.dk>
2023-01-12 11:15:27 +0100

    Update create_database.sql
    Removed definition of keys

--- create_database.sql
+++ create_database.sql
@@ -10,7 +10,7 @@ GO
 
 /****** Table [Orders] ******/
 CREATE TABLE [Orders](
-	[Id] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Id] [int] NOT NULL,
 	[Date] [datetime] NOT NULL,
 	[Status] [nvarchar](50) DEFAULT (N'New') NOT NULL,
 	[Discount] [int] DEFAULT ((0)) NOT NULL,
@@ -20,7 +20,7 @@ GO
 
 /****** Table [Products] n******/
 CREATE TABLE [Products](
-	[Id] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Id] [int] NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
 	[Description] [nvarchar](max) NULL,
 	[Price] [decimal](18, 2) NULL
@@ -29,13 +29,8 @@ GO
 
 /****** Table [OrderLines] ******/
 CREATE TABLE [OrderLines](
-	[OrderId] [int] FOREIGN KEY REFERENCES [Orders] ([Id]) NOT NULL,
-	[ProductId] [int] FOREIGN KEY REFERENCES [Products] ([Id]) NOT NULL,
+	[OrderId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
 	[Quantity] [int] NOT NULL,
-	CONSTRAINT [PK_OrderLines] PRIMARY KEY CLUSTERED 
-	(
-		[OrderId] ASC,
-		[ProductId] ASC
-	)
 ) 
 GO
